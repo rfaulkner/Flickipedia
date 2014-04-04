9a8f17b67351929b5a7b4513547c063c5a1a3a99
rfaulk <rfaulk@yahoo-inc.com>
2014-04-04 00:31:38 -0700

    mod - refactor and remove "See Also" & "External links" sections

--- flickipedia/parse.py
+++ flickipedia/parse.py
@@ -1,17 +1,26 @@
 import re
 
+
 def parse(lines):
     out = ''
+    section = None
     for line in lines:
         if re.search(r'^== ', line):
-            out += '<br><h3>' + ''.join(line.strip().split('==')[1:-1]).strip() \
-                   + '</h3>'
+            formatted, section = set_heading(line, '==', 'h3')
         elif re.search(r'^=== ', line):
-            out += '<br><h4>' + ''.join(line.strip().split('===')[1:-1]).strip() \
-                   + '</h4>'
+            formatted, section = set_heading(line, '===', 'h4')
         elif re.search(r'^==== ', line):
-            out += '<br><h4>' + ''.join(line.strip().split('====')[1:-1]).strip() \
-                   + '</h4>'
+            formatted, section = set_heading(line, '====', 'h4')
         else:
-            out += '<p>' + line.strip() + '</p>'
+            formatted = '<p>' + line.strip() + '</p>'
+
+        if not section == "seealso" and not section == "externallinks":
+            out += formatted
     return out
+
+
+def set_heading(line, token, heading_markup):
+    section = ''.join(line.strip().split(token)[1:-1]).strip()
+    out = '<br><{0}>'.format(heading_markup) + section + \
+          '</{0}>'.format(heading_markup)
+    return out, ''.join(section.lower().split())
