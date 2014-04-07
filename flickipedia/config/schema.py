b9b721f1ec29cda45bd57131b88b5aa2cb07b0d2
rfaulk <rfaulk@yahoo-inc.com>
2014-04-06 23:12:39 -0700

    mod - schema for User table.

---
+++ flickipedia/config/schema.py
@@ -0,0 +1,27 @@
+"""
+Ryan Faulkner, 2014
+
+Schema definitions for sqlalchemy
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+
+    __tablename__ = 'Users'
+
+    uid = Column(Integer, primary_key=True, autoincrement=True)
+    handle = Column(String)
+    email = Column(String(24))
+    firstname = Column(String(24))
+    lastname = Column(String(24))
+    password = Column(String(24))
+    date_join = Column(Integer)
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+            self.name, self.fullname, self.password)
