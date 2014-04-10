bd734c9a85935ebdc1de6ecb5da40d7a7cdd43d1
rfaulk <rfaulk@yahoo-inc.com>
2014-04-10 15:01:21 -0700

    add - parse method for substituting local links

--- flickipedia/parse.py
+++ flickipedia/parse.py
@@ -1,16 +1,33 @@
 import re
+
 from BeautifulSoup import BeautifulSoup
+from flickipedia.config.settings import GET_VAR_ARTICLE
 
 
 def parse_strip_elements(html):
+    """
+    Strips out undesirable divs & spans
+    """
     soup = BeautifulSoup(html)
 
-    #
+    # Remove thumbs
     for div in soup.findAll('div', 'thumbinner'): div.extract()
     for div in soup.findAll('div', 'thumb tright'): div.extract()
 
-    #
+    # Remove edit links
     for div in soup.findAll('span', 'mw-editsection'): div.extract()
     return str(soup)
 
 
+def parse_convert_links(html):
+    """
+    Handles converting links to Flickipedia
+    """
+    # TODO - get all the links
+
+    soup = BeautifulSoup(html)
+
+    for a in soup.findAll('a', 'mw-redirect'):
+        a['href'] = a['href'].replace('/wiki/', '?' + GET_VAR_ARTICLE + '=')
+
+    return str(soup)
