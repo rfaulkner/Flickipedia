02f2e353307457132516673125a841b507dfc7a1
rfaulk <rfaulk@yahoo-inc.com>
2014-04-11 20:57:58 -0700

    mv - login manager to web initialization.

--- flickipedia/web/__init__.py
+++ flickipedia/web/__init__.py
@@ -3,7 +3,15 @@ __author__ = 'rfaulk'
 from flickipedia.config import settings
 from flask import Flask
 
+import os
+from flask.ext.login import LoginManager
+
+
 # Instantiate flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = settings.__secret_key__
 app.config['VERSION'] = settings.__version__
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
