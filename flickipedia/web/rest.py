b7492aa598aad9c02a02549f0a5137e6f1b2e7e2
rfaulk <rfaulk@yahoo-inc.com>
2014-05-04 16:51:17 -0700

    mod - updates to api_get_glyph

--- flickipedia/web/rest.py
+++ flickipedia/web/rest.py
@@ -5,38 +5,48 @@ Defines restful interface to backend
 
 from flickipedia.mysqlio import DataIOMySQL
 from flickipedia.config import schema
+from flickipedia.config import log
 
 
-def set_glyph(user, photo_id, article_id):
+def api_set_glyph(user, photo_id, article_id):
     """ Toggles the like-glyph value for the given triplet """
 
     io = DataIOMySQL()
     io.connect()
 
-    curr = get_glyph(user, photo_id, article_id)
+    result = api_get_glyph(user, photo_id, article_id)
 
     # toggle and set new value (delete row if it doesn't exist)
-    if curr:
+    if result:
         # io.update false
         pass
     else:
         # io.update true
         pass
 
-    # get glyph current state
 
     raise NotImplementedError()
     # return
 
-def get_glyph(user, photo_id, article_id):
+def api_get_glyph(uid, pid, aid):
     """ Determines the like-glyph value for the given triplet """
     io = DataIOMySQL()
     io.connect()
-
     schema_obj = getattr(schema, 'Likes')
-    return io.session.query(schema_obj, schema_obj.is_set).filter(
-        schema_obj.photo_id == photo_id,
-        schema_obj.article_id == article_id,
-        schema_obj.user_id == user
-    )[0]
+
+    # Query to extract
+    res = io.session.query(schema_obj, schema_obj.is_set).filter(
+        schema_obj.photo_id == pid,
+        schema_obj.article_id == aid,
+        schema_obj.user_id == uid
+    ).limit(1).all()
+
+    if len(res) == 0:
+        log.error('REST \'api_get_glyph\': Couldn\'t find ('
+                  'user="%s", photo_id=%s, article_id=%s)' % (
+            uid, pid, aid))
+        return None
+    else:
+        return res[0]
+
 
