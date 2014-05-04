6f09aab4b2e7c7b02c9ead230fdff14aea2e4467
rfaulk <rfaulk@yahoo-inc.com>
2014-05-04 16:50:04 -0700

    mod - updates to service rdbms schema

--- flickipedia/config/schema.py
+++ flickipedia/config/schema.py
@@ -11,10 +11,11 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ Elements for Flickipedia users """
 
     __tablename__ = 'Users'
 
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    _id = Column(Integer, primary_key=True, autoincrement=True)
     handle = Column(String(32))
     email = Column(String(24))
     firstname = Column(String(24))
@@ -28,13 +29,44 @@ class User(Base):
 
 
 class Photo(Base):
+    """ Elements for Flickr Photos """
 
     __tablename__ = 'Photos'
 
-    photo_id = Column(Integer, primary_key=True, autoincrement=True)
-    article_name = Column(String(32))
+    _id = Column(Integer, primary_key=True, autoincrement=True)
+    flickr_id = Column(Integer)
+    article_id = Column(Integer)
     votes = Column(Integer)
 
     def __repr__(self):
         return "<Photo(photo='%s', article='%s', votes='%s')>" % (
             self.photo, self.article, self.votes)
+
+
+class Article(Base):
+    """ Elements for wiki articles """
+
+    __tablename__ = 'Photos'
+
+    _id = Column(Integer, primary_key=True, autoincrement=True)
+    wiki_aid = Column(Integer)
+    article_name = Column(String(32))
+
+    def __repr__(self):
+        return "<Article(name='%s', article='%s', votes='%s')>" % (
+            self.article_name, self.article, self.votes)
+
+
+class Like(Base):
+    """ Elements for likes on article photos for flickr """
+
+    __tablename__ = 'Likes'
+
+    _id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer)
+    flickr_pid = Column(Integer)
+    article_id = Column(Integer)
+
+    def __repr__(self):
+        return "<Like(user='%s', flickr_id='%s', article_id='%s')>" % (
+            self.user_id, self.flickr_pid, self.article_id)
