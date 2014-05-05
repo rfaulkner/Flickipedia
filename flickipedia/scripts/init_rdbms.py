c432fb1ec250a194cb9cac8538225e1460170aef
rfaulk <rfaulk@yahoo-inc.com>
2014-05-04 17:14:16 -0700

    add - automate table creation.

---
+++ flickipedia/scripts/init_rdbms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+
+Ryan Faulkner 2014
+
+"""
+
+from flickipedia.mysqlio import DataIOMySQL
+
+
+def main():
+    io = DataIOMySQL()
+    io.connect()
+
+    io.create_table('User')
+    io.create_table('Photo')
+    io.create_table('Article')
+    io.create_table('Like')
+
+
+def cli():
+    exit(main())
+
+
+if __name__ == '__main__':
+    cli()
+
