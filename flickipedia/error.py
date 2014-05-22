319b47b61540d6d229c735dfe8382724d77fa3d5
rfaulk <rfaulk@yahoo-inc.com>
2014-05-22 10:57:25 -0700

    add - template & option members to exception classes

--- flickipedia/error.py
+++ flickipedia/error.py
@@ -4,11 +4,17 @@ This module defines exception classes for flickipedia
 
 class WikiAPICallError(Exception):
     """ Basic exception class for Wiki api calls """
-    def __init__(self, message="Wiki api call error."):
+    def __init__(self, message="Wiki api call error.", template="index.html",
+                 options=None):
         Exception.__init__(self, message)
+        self.template = template
+        self.options = options
 
 
 class FlickrAPICallError(Exception):
     """ Basic exception class for Flickr api calls"""
-    def __init__(self, message="Flickr api call error."):
+    def __init__(self, message="Flickr api call error.", template="index.html",
+                 options=None):
         Exception.__init__(self, message)
+        self.template = template
+        self.options = options
