34b0eac160180e7847a695e47f217b8d29131032
rfaulk <rfaulk@yahoo-inc.com>
2014-05-26 19:35:57 -0700

    add - model for exclude interface

---
+++ flickipedia/model/exclude.py
@@ -0,0 +1,35 @@
+"""
+Exclude model class
+"""
+
+from flickipedia.config import log, schema
+from flickipedia.mysqlio import DataIOMySQL
+
+
+class ExcludeModel(object):
+
+    def __init__(self):
+        super(ExcludeModel, self).__init__()
+
+        self.io = DataIOMySQL()
+        self.io.connect()
+
+    def get_exclude(self, user_id, article_id, photo_id):
+        """ Retrieve whether an object has been voted exclude """
+        schema_obj = getattr(schema, 'Exclude')
+        res = self.io.session.query(schema_obj).filter(
+            schema_obj.user_id == user_id,
+            schema_obj.article_id == article_id,
+            schema_obj.photo_id == photo_id,
+        ).all()
+        if len(res) > 0:
+            return res[0]
+        else:
+            return None
+
+    def insert_like(self, user_id, article_id, photo_id):
+        return self.io.insert('Exclude', user_id=user_id,
+                              article_id=article_id, photo_id=photo_id)
+
+    def delete_like(self, like_obj):
+        return self.io.delete(like_obj)
