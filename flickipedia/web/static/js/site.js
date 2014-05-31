cea9eb2ca6cc333d34ff895ffe23ae8d7aa15e2e
rfaulk <rfaulk@yahoo-inc.com>
2014-05-30 17:38:48 -0700

    mod - cleanup the logic and implementation of handling glyph endorsement and link beacons

--- flickipedia/web/static/js/site.js
+++ flickipedia/web/static/js/site.js
@@ -6,6 +6,35 @@
 
 
 /**
+ * Builds the glyph-image element based on state
+ *
+ * @param path          relative path of image file
+ * @param isHover       is the glyph element under the pointer?
+ * @param isSelected    has this element been selected by the user?
+ * @param width         [optional] width of glyph
+ * @param height        [optional] height of glyph
+ *
+ * @returns {string}    Markup for the glyph image element
+ */
+function getGlyphImageState(path, isHover, isSelected, width, height) {
+
+    if (width == undefined) { width = 25; }
+    if (height== undefined) { height = 25; }
+
+    var sizeStyle = 'width="' + width + '" height="' + height + '">';
+
+    if (!isHover && !isSelected) {
+        return '<img style="float:left; opacity:0.4; background-color:#cccccc;" src="/static/img/' + path + '"' + sizeStyle;
+    } else if (isHover && !isSelected) {
+        return '<img style="float:left; opacity:0.6; background-color:#cccccc;" src="/static/img/' + path + '"' + sizeStyle;
+    } else if (!isHover && isSelected) {
+        return '<img style="float:left; opacity:0.8; background-color:#cccccc;" src="/static/img/' + path + '"' + sizeStyle;
+    } else {
+        return '<img style="float:left; opacity:0.8; background-color:#cccccc;" src="/static/img/' + path + '"' + sizeStyle;
+    }
+}
+
+/**
  * Class object for handling callback code
  *
  * @param photos
@@ -38,15 +67,12 @@ function InitPageCallbacks(
         $("#" + sectionImageHandle + "-" + idx).hover(function() {
 
             var linkGlyph = $(this).find("a");
-            linkGlyph[0].innerHTML = '<img style="opacity:0.8; background-color:#cccccc;" src="/static/img/link.png" width="25" height="25">';
+            linkGlyph[0].innerHTML = getGlyphImageState('link.png', true, false);
 
             if (!onVoteGlyph[idx]) {
 
                 var endorseGlyph = $(this).find("div.endorse");
                 var excludeGlyph = $(this).find("div.exclude");
-
-                var imgEndorse = '<img style="float:left; opacity:0.4; background-color:#cccccc;" src="/static/img/endorse.png" width="25" height="25">';
-                var imgExclude = '<img style="float:left; opacity:0.4; background-color:#cccccc;" src="/static/img/unendorse.png" width="25" height="25">';
 
                 // Determine whether this user likes the photo
                 $.getJSON('rest/api_photo_endorse_fetch' + '?' + params, function(data) {
@@ -56,19 +82,12 @@ function InitPageCallbacks(
                     isExcluded[idx] = parseInt(data['exclude-fetch']);
                 });
 
-                if (isEndorsed[idx]) {
-                    imgEndorse = '<img style="float:left; opacity:0.7; background-color:#cccccc;" src="/static/img/endorse.png" width="25" height="25">';
-                }
-
-                if (isExcluded[idx]) {
-                    imgExclude = '<img style="float:left; opacity:0.7; background-color:#cccccc;" src="/static/img/unendorse.png" width="25" height="25">';
-                }
-                endorseGlyph[0].innerHTML = imgEndorse;
-                excludeGlyph[0].innerHTML = imgExclude;
+                endorseGlyph[0].innerHTML = getGlyphImageState('endorse.png', false, isEndorsed[idx]);
+                excludeGlyph[0].innerHTML = getGlyphImageState('unendorse.png', false, isExcluded[idx]);
             }
         }, function() {
             var linkGlyph = $(this).find("a");
-            linkGlyph[0].innerHTML = '<img style="opacity:0.4; background-color:#cccccc;" src="/static/img/link.png" width="25" height="25">';
+            linkGlyph[0].innerHTML = getGlyphImageState('link.png', true, false);
 
             if (!onVoteGlyph[idx]) {
                 var endorseGlyph = $(this).find("div.endorse");
@@ -86,14 +105,11 @@ function InitPageCallbacks(
     this.voteGlyphImageHover = function(idx) {
         $("#vote-glyph-" + idx).hover(function() {
             if (!onVoteGlyph[idx]) {
-                var imgEndorse = '<img style="float:left; opacity:0.6; background-color:#cccccc;" src="/static/img/endorse.png" width="25" height="25">';
-                var imgExclude = '<img style="float:left; opacity:0.6; background-color:#cccccc;" src="/static/img/unendorse.png" width="25" height="25">';
-
                 var endorseGlyph = $(this).find("div.endorse");
                 var excludeGlyph = $(this).find("div.exclude");
 
-                endorseGlyph[0].innerHTML = imgEndorse;
-                excludeGlyph[0].innerHTML = imgExclude;
+                endorseGlyph[0].innerHTML = getGlyphImageState('endorse.png', true, isEndorsed[idx]);
+                excludeGlyph[0].innerHTML = getGlyphImageState('unendorse.png', true, isEndorsed[idx]);
 
                 onVoteGlyph[idx] = true;
             }
@@ -119,6 +135,7 @@ function InitPageCallbacks(
         $("#endorse-" + idx).click(function() {
             $.getJSON('rest/api_photo_endorse_event' + '?' + params, function(data) {
                 isEndorsed[idx] = isEndorsed[idx] ? false : true;
+                // CALL THE HOVER FUNCTION HERE FOR THE CLICKED ELEM
             });
         });
     };
@@ -134,6 +151,7 @@ function InitPageCallbacks(
         $("#exclude-" + idx).click(function() {
             $.getJSON('rest/api_photo_exclude_event' + '?' + params, function(data) {
                 isExcluded[idx] = isExcluded[idx] ? false : true;
+                // CALL THE HOVER FUNCTION HERE FOR THE CLICKED ELEM
             });
         });
     };
