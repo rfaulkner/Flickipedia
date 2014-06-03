3747c8d0f3cb3ac4fd0954cdf09555cb4a66f3ec
rfaulk <rfaulk@yahoo-inc.com>
2014-06-03 16:59:33 -0700

    add - test path and dummy test for ranking

---
+++ flickipedia/tests/test_rank.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+    git-deploy.testsuite
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2014 by Ryan Faulkner.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import unittest
+
+
+class TestRank(unittest.TestCase):
+    """ Test cases for GitDeploy initialization and config """
+    def test_order_photos_by_rank(self):
+        # Choose a test article
+        # Build endorsements and exclusions around photo results
+        # ensure that returned order is correct
+        self.assertTrue(False)
