0b94536138c41ab3e4a067c5ae51abac341ab6bf
rfaulk <rfaulk@yahoo-inc.com>
2014-06-03 16:55:53 -0700

    add - implementation of order_photos_by_rank

--- flickipedia/rank.py
+++ flickipedia/rank.py
@@ -5,18 +5,18 @@ This module handles functionality around entity ranking
 from flickipedia.model.likes import LikeModel
 from flickipedia.model.exclude import ExcludeModel
 
+
 def order_photos_by_rank(article_id, photos):
     """ Reorders photos by score """
     lm = LikeModel()
     em = ExcludeModel()
 
-    # TODO - fill in computation & sort logic
     # Compute scores
-    for photo in photos:
-        # Get Exclusions
+    for i in xrange(len(photos)):
+        # Get Exclusions & Endorsements
+        exclusions = em.get_excludes_article_photo(article_id, photos['id'])
+        endorsements = lm.get_likes_article_photo(article_id, photos['id'])
+        photos[i]['score'] = len(endorsements) - len(exclusions)
 
-        # Get Endorsements
-        # compute score
-        pass
-    # Apply sort
-    return photos
+    f = lambda x: x['score']
+    return sorted(photos, f)
