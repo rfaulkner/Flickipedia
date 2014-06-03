c56f940fe51b4aa46fc5569334eb74855ac0f141
rfaulk <rfaulk@yahoo-inc.com>
2014-06-02 17:24:18 -0700

    fix - no dependence on user

--- flickipedia/rank.py
+++ flickipedia/rank.py
@@ -2,12 +2,19 @@
 This module handles functionality around entity ranking
 """
 
-def order_photos_by_rank(article_id, user_id, photos):
+from flickipedia.model.likes import LikeModel
+from flickipedia.model.exclude import ExcludeModel
+
+def order_photos_by_rank(article_id, photos):
     """ Reorders photos by score """
+    lm = LikeModel()
+    em = ExcludeModel()
+
     # TODO - fill in computation & sort logic
     # Compute scores
     for photo in photos:
         # Get Exclusions
+
         # Get Endorsements
         # compute score
         pass
