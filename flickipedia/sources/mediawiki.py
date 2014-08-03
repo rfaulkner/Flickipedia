f94f29b460945485554f8f1b278d439f9c8c9f36
rfaulk <rfaulk@yahoo-inc.com>
2014-08-03 14:17:51 -0700

    mod - convert oauth storage patterns to object serialization. Since we're dealing with python objects use cPickle rather than redis

--- flickipedia/sources/mediawiki.py
+++ flickipedia/sources/mediawiki.py
@@ -8,6 +8,39 @@ Mediawiki oauth glue
 from flickipedia.redisio import DataIORedis, hmac
 from mwoauth import ConsumerToken, Handshaker
 from flickipedia.config import settings
+import cPickle
+
+
+def getPicklerFilename(type, key):
+    """ Formats the pickle filename
+    :param type:    object type to pickle
+    :param key:     unique id
+    :return:        string filename
+    """
+    return settings.SERVER_ROOT + '/' + \
+           settings.MWOAUTH_PKL_PATH + '/' + \
+           type + '_' + key
+
+
+def setSerialized(obj, type, key):
+    """
+    :param obj:
+    :param type:
+    :param key:
+    :return:
+    """
+    with open(getPicklerFilename(type, key), 'wb') as f:
+        cPickle.dump(obj, f)
+
+
+def getSerialized(type, key):
+    """
+    :param type:
+    :param key:
+    :return:
+    """
+    with open(getPicklerFilename(type, key), 'rb') as f:
+        return cPickle.load(f)
 
 
 def getMWRedirect(user):
@@ -25,17 +58,16 @@ def getMWRedirect(user):
     # Step 1: Initialize -- ask MediaWiki for a temporary key/secret for user
     redirect, request_token = handshaker.initiate()
 
-    # Store request_token in redis
-    # TODO - ensure user and request_token are properly serialized
+    # Pickle the handshaker & request token
     key = hmac(user)
-    DataIORedis().connect()
-    DataIORedis().write(key, request_token)
+    setSerialized(handshaker, settings.MWOAUTH_HANDSHAKER_PKL_KEY, key)
+    setSerialized(request_token, settings.MWOAUTH_REQTOKEN_PKL_KEY, key)
 
     # Step 2: Authorize -- send user to MediaWiki to confirm authorization
     return redirect
 
 
-def getMWAccessToken(user, handshaker, response_query_string):
+def getMWAccessToken(user, response_query_string):
     """Generate Access token from MW auth query string + access token
     :param user:                    string, user
     :param handshaker:              Handshakerobject from mwoauth
@@ -44,26 +76,28 @@ def getMWAccessToken(user, handshaker, response_query_string):
     """
     key = hmac(user)
 
-    # Obtain authorized key/secret for "resource owner"
-    request_token = DataIORedis().read(key)
-    access_token = handshaker.complete(request_token, response_query_string)
+    # unpickle the handshaker
+    handshaker = getSerialized(settings.MWOAUTH_HANDSHAKER_PKL_KEY, key)
+    req_token = getSerialized(settings.MWOAUTH_REQTOKEN_PKL_KEY, key)
 
-    # Put access token in redis - ovewrite request token
-    key = hmac(user)
-    DataIORedis().connect()
-    DataIORedis().write(key, request_token)
+    # Obtain authorized key/secret for "resource owner"
+    access_token = handshaker.complete(req_token, response_query_string)
+
+    # Serialize access token in redis - ovewrite request token
+    setSerialized(access_token, settings.MWOAUTH_ACCTOKEN_PKL_KEY, key)
 
     return access_token
 
 
-def getMWidentity(user, handshaker):
+def getMWidentity(user):
     """Get identifying information about the user
     :param user:            string, user
     :param handshaker:      Handshakerobject from mwoauth
     :return:                MW identity object
     """
     key = hmac(user)
-    access_token = DataIORedis().read(key)
+    handshaker = getSerialized(settings.MWOAUTH_HANDSHAKER_PKL_KEY, key)
+    access_token = getSerialized(settings.MWOAUTH_ACCTOKEN_PKL_KEY, key)
     return handshaker.identify(access_token)
 
 
